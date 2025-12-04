083daa4a31d9c604b0d24148f04a3ddea184cef1
kastashenkova <astashenkova.katya@gmail.com>
2025-12-04 14:19:44 +0200

    created checked exception

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,20 @@
 package core.basesyntax;
 
 public class UserService {
+    private static final String message = "Your passwords are incorrect. Try again.";
+    private final PasswordValidator validator = new PasswordValidator();
+
     public void registerUser(User user) {
-        //write your code here
+        try {
+            validator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
+        } catch (PasswordValidationException e) {
+            try {
+                throw new PasswordValidationException(message);
+            } catch (PasswordValidationException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
     }
 
     public void saveUser(User user) {
