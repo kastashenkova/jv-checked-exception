2a308017b68ed48b96e5e2f1827b29115e9d27b8
kastashenkova <astashenkova.katya@gmail.com>
2025-12-04 14:55:44 +0200

    fixed validator null core

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,10 +5,9 @@ public class PasswordValidator {
 
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        if (password != null && repeatPassword != null) {
-            if (!password.equals(repeatPassword) || password.length() < 10) {
-                throw new PasswordValidationException(message);
-            }
+        if (!password.equals(repeatPassword) || password.length() < 10
+                || password == null || repeatPassword == null) {
+            throw new PasswordValidationException(message);
         }
     }
 }
